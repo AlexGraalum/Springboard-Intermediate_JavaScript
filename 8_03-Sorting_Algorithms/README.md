2747583c5f81240c56b38853022237f254834dfd
Alex Graalum <alexgraalum@yahoo.com>
2024-07-26 16:13:37 -0700

    Merge Sort Complete

--- 8_03-Sorting_Algorithms/README.md
+++ 8_03-Sorting_Algorithms/README.md
@@ -4,7 +4,7 @@
 | Assignment     | File                           | Status          |
 | -------------- | ------------------------------ | --------------- |
 | Bubble Sort    | [bubble.js](./bubble.js)       | <b>Complete</b> |
-| Merge Sort     | [merge.js](./merge.js)         | <b>Planned</b>  |
+| Merge Sort     | [merge.js](./merge.js)         | <b>Complete</b> |
 | Insertion Sort | [insertion.js](./insertion.js) | <b>Planned</b>  |
 | Selection Sort | [selection.js](./selection.js) | <b>Planned</b>  |
 | Quick Sort     | [quick.js](./quick.js)         | <b>Planned</b>  |
--- 8_03-Sorting_Algorithms/merge.js
+++ 8_03-Sorting_Algorithms/merge.js
@@ -1,5 +1,37 @@
-function merge() {}
+function merge(arr1, arr2) {
+    const arr = [];
+    let i = 0;
+    let j = 0;
 
-function mergeSort() {}
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            arr.push(arr1[i]);
+            i++;
+        } else {
+            arr.push(arr2[j]);
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        arr.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        arr.push(arr2[j]);
+        j++;
+    }
 
-module.exports = { merge, mergeSort};
+    return arr;
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+
+    return merge(left, right);
+}
+
+module.exports = { merge, mergeSort };
