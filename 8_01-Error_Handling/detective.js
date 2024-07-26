92c8697a2da64a3b0fcef855217fcdf1f77efa21
Alex Graalum <alexgraalum@yahoo.com>
2024-07-26 14:43:55 -0700

    Detective.js complete

--- 8_01-Error_Handling/detective.js
+++ 8_01-Error_Handling/detective.js
@@ -1,13 +1,33 @@
-function mysteryOperation ()
-{
+function mysteryOperation() {
 	const outcome = Math.random(); // Generates a random number between 0 and 1.
 
-	if (outcome < 0.5)
-	{
+	if (outcome < 0.5) {
 		console.log("The operation is completed successfully!");
 	}
-	else
-	{
+	else {
 		throw new Error("The operation is failed mysteriously!");
 	}
 }
+
+const success = 13;
+const failure = 1;
+const attend = 3;
+
+const mission_count = 20;
+
+let vacation_days = 0;
+
+for (let i = 0; i < mission_count; i++) {
+	try {
+		mysteryOperation();
+		vacation_days += attend;
+	}
+	catch (error) {
+		vacation_days += failure;
+	}
+	finally {
+		vacation_days += success;
+	}
+}
+
+console.log(vacation_days);
