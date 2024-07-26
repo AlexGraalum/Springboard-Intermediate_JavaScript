8e1c6dc529b4fc592043227f30f3cf75822cc69c
Alex Graalum <alexgraalum@yahoo.com>
2024-07-26 16:26:48 -0700

    Selection Sort Complete

--- 8_03-Sorting_Algorithms/selection.js
+++ 8_03-Sorting_Algorithms/selection.js
@@ -1,3 +1,18 @@
-function selectionSort() {}
+function selectionSort(arr) {
+    let size = arr.length;
+    for (let i = 0; i < size - 1; i++) {
+        let min = i;
+
+        for (let j = i + 1; j < size; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        let temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
 
 module.exports = selectionSort;
