68e9c8efdcfc880cdbbce7471856dffe0067ced5
Alex Graalum <alexgraalum@yahoo.com>
2024-07-26 17:47:11 -0700

    Radix Sort Complete

--- 8_03-Sorting_Algorithms/README.md
+++ 8_03-Sorting_Algorithms/README.md
@@ -8,4 +8,4 @@
 | Insertion Sort | [insertion.js](./insertion.js) | <b>Complete</b> |
 | Selection Sort | [selection.js](./selection.js) | <b>Complete</b> |
 | Quick Sort     | [quick.js](./quick.js)         | <b>Complete</b> |
-| Custom Sort    | [radix.js](./radix.js)         | <b>Planned</b>  |
+| Radix Sort     | [radix.js](./radix.js)         | <b>Complete</b> |
--- 8_03-Sorting_Algorithms/radix.js
+++ 8_03-Sorting_Algorithms/radix.js
@@ -1,3 +1,38 @@
-function radixSort() {}
+function getDigit(num, digit) {
+    return Math.floor((num / Math.pow(10, digit)) % 10);
+}
 
-module.exports = radixSort;
+function digitCount(num) {
+    let i = 0;
+
+    while (num > 0) {
+        i++;
+        num = ((num - (num % 10)) / 10);
+    }
+    return i;
+}
+
+function mostDigits(arr) {
+    let i = 0;
+
+    for (a of arr)
+        i = Math.max(i, digitCount(a));
+    return i;
+}
+
+function radixSort(arr) {
+    for (let i = 0; i < mostDigits(arr); i++) {
+        let buckets = Array.from({ length: 10 }, () => []);
+
+        for (let j = 0; j < arr.length; j++) {
+            let digit = getDigit(arr[j], i);
+
+            buckets[digit].push(arr[j]);
+        }
+
+        arr = [].concat(...buckets);
+    }
+    return arr;
+}
+
+module.exports = { getDigit, digitCount, mostDigits, radixSort };
